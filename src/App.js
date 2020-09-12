e3c9011f3e8ee48e25db625a41c680eeb5615c72
Mariugo Megiani <mariugomegiani@gmail.com>
2020-09-11 21:55:42 -0300

    First commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, {Fragment} from 'react';
+import './App.css';
+
+//Componentes
+import Name from "./components/Name";
+
+function App() {
+  return (
+    <Fragment>
+    <div className="container" >
+    <Name/>
+    </div>
+    </Fragment>
+  );
+}
+
+export default App;
---
+++ src/components/Name.js
@@ -0,0 +1,13 @@
+import React, {Fragment} from "react";
+
+const Name = () => {
+    return (
+    <Fragment><div className="jumbotron">
+    <h1 className="display-4">Mariugo Megiani</h1>
+    <p className="lead">Este é o projeto que será utilizado Spring Boot, PGSQL e REACT JS</p>
+    </div>
+    </Fragment>
+    );
+};
+
+export default Name;
